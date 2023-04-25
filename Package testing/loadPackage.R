cd75b63a2963764ff33d2607edd3868a1ec31f1a
jaredvdb <98898004+jmvdb5@users.noreply.github.com>
2023-04-25 14:42:32 -0400

    Testing Files

--- Package testing/loadPackage.R
+++ Package testing/loadPackage.R
@@ -9,4 +9,26 @@ library(starFunctions)
 
 data("starData")
 
-test <- starDist(starData$Luminosity[1],starData$Absolute.Magnitude[1])
+# Using starDistMainSeq() 
+test1 <- starDistMainSeq(0.0005, 16.6)
+test1
+
+# Using starDistMainSeq() with the dataset
+test2 <- starDistMainSeq(starData$Luminosity[1],starData$Absolute.Magnitude[1])
+test2
+
+# Using starDist() 
+test3 <- starDist(3.846e26, 5778, 4.83, -0.0452)
+test3
+
+# Using starDistMainSeq() 
+bolCorrApprox(8800)
+
+test4 <- bolCorrApprox(2800)
+test4
+
+# Using starSurfaceArea() 
+starSurfaceArea(0.16)
+
+test5 <- starSurfaceArea(0.102)
+test5
