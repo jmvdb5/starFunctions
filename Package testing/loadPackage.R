ba8d459ccf20c6f4361f0de5f06941e39332e9e2
jaredvdb <98898004+jmvdb5@users.noreply.github.com>
2023-04-25 14:41:05 -0400

    Testing Files

--- Package testing/loadPackage.R
+++ Package testing/loadPackage.R
@@ -1,8 +1,8 @@
 # Load devtools
 library(devtools)
 
-# Install the package from the compressed file
-install_local("starFunctions_0.2.0.tar.gz")
+# Install the package from the compressed file, BE SURE TO CHECK THE RELEASE NUMBER
+install_local("starFunctions_0.2.1.tar.gz")
 
 # Load the package
 library(starFunctions)
